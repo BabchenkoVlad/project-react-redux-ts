ea28414cbd992eca7efe77c2848f114fbd6e809f
BabchenkoVlad <babch.vl@gmail.com>
2023-11-06 16:25:10 +0200

    reducer

---
+++ src/state/reducers/repositoriesReducers.ts
@@ -0,0 +1,41 @@
+interface IReposiroriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+interface ISearchRepositoriesAction {
+  type: 'search_repositories';
+}
+
+interface ISearchRepositoriesSuccessAction {
+  type: 'search_repositories_success';
+  payload: string[];
+}
+
+interface ISearchRepositoriesErrorAction {
+  type: 'search_repositories_error';
+  payload: string;
+}
+
+const reducer = (
+  state: IReposiroriesState,
+  action:
+    ISearchRepositoriesAction |
+    ISearchRepositoriesSuccessAction |
+    ISearchRepositoriesErrorAction
+): IReposiroriesState => {
+
+  switch (action.type) {
+    case 'search_repositories':
+      return { loading: true, error: null, data: [] };
+    case 'search_repositories_success':
+      return { loading: false, error: null, data: action.payload };
+    case 'search_repositories_error':
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
+
+export default reducer
